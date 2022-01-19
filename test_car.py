8117384fb278e1ee101ae1db53cc3ccb1df1c447
CECNL <90022378+CECNL@users.noreply.github.com>
2022-01-19 18:42:19 +0800

    Update test_car.py

--- test_car.py
+++ test_car.py
@@ -2,19 +2,21 @@ import keyboard
 import serial
 import time
 
+
+#### port"COM5"  must be change to suitable port
 ser = serial.Serial("COM5", 9600, timeout = 1)
 
 while(True):
     if keyboard.is_pressed('w'):
-        print("�?)
+        print("ä¸?)
         ser.write(b'1')
         time.sleep(0.1)
     elif keyboard.is_pressed('s'):
-        print("�?)
+        print("ä¸?)
         ser.write(b'2')
         time.sleep(0.1)
     elif keyboard.is_pressed('a'):
-        print("�?)
+        print("å·?)
         ser.write(b'3')
         time.sleep(0.1)
     elif keyboard.is_pressed('d'):
